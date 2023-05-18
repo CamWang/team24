914a68ff5769e0cd79bb07ebb809c97b62f7d44c
Yaoyang Ding <ydin0039@student.monash.edu>
2023-05-19 05:08:54 +1000

    Fix bugs

--- src/main/java/fit5171/monash/edu/entity/Flight.java
+++ src/main/java/fit5171/monash/edu/entity/Flight.java
@@ -14,7 +14,7 @@ public class Flight {
     private String company;
     private Timestamp dateFrom;
     private Timestamp dateTo;
-    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+    public final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
     Airplane airplane;
 
     public Flight() {
